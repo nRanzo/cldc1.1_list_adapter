d64419208621ade33059571747192422ef507592
Nicola Ranzolin <104451140+nRanzo@users.noreply.github.com>
2024-06-15 18:36:39 +0200

    Update testrun.sh
    changed language in EN and removed an unuseful comment

--- testrun.sh
+++ testrun.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 
-# Funzione per ottenere la versione da un file JAR
+# Function to obtain the version from a JAR file
 get_version() {
     jar_file=$1
     version=$(unzip -p $jar_file META-INF/MANIFEST.MF | grep 'Implementation-Version' | cut -d' ' -f2)
     echo "Implementation-Version: $version"
 }
 
-# Mostro le versioni delle librerie JUnit
+# Schowing versions of the JUnit libraries
 echo "Mostrando le versioni delle librerie JUnit e Hamcrest-Core:"
 get_version "JUnit/junit-4.13.2.jar"
 get_version "JUnit/hamcrest-core-1.3.jar"
 
-# Compilo i file Java
+# Compiling Java files
 javac -cp .:JUnit/junit-4.13.2.jar:JUnit/hamcrest-core-1.3.jar myAdapter/*.java myTest/*.java
 
-# Eseguo i test
+# Running Tests
 java -cp .:JUnit/junit-4.13.2.jar:JUnit/hamcrest-core-1.3.jar myTest.TestRunner
-
-# Messaggi di errore o successo stampati da TestRunner.java
